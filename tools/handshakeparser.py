2e78473020e4d9d32471154236080acedc07448c
Codeboybebop <codeboy.bebop@gmail.com>
2023-05-09 22:43:50 -0500

    Fixed handshakeparser.py tests

--- tools/handshakeparser.py
+++ tools/handshakeparser.py
@@ -251,7 +251,7 @@ async def test_server_hello_done():
 async def test_client_key_exchange():
     reader = HandshakeParser()
 
-    key_exchange_data =     """
+    key_exchange_data = """
         30819404282536556CCDAC34914FD115 4C2A9F9E5D7FDE774350FD66907A2021 A9A1DF8C982F30CF2BE4CF91AF306830 2106082A85030701010101301506092A 850307010201010106082A8503070101 020203430004408D490F4CB030E23974 E218C2787312BED9F361377CCCF52A7E
         73856C2A19D98600CEC1B836C6405B24 1BA7CD8C085E2DEC6C4E0A61D972F1D9 8FE4B8760E1971
         """
@@ -260,9 +260,7 @@ async def test_client_key_exchange():
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
     expected = Variant \
-        (0x10, key_exchange_data)
-
-    print(res)
+        (0x10, bytearray.fromhex(key_exchange_data))
 
     compare_result(res, expected)
 
@@ -270,11 +268,11 @@ async def test_client_key_exchange():
 async def test_certificate_verify():
     reader = HandshakeParser()
 
-    mybuffer = bytes.fromhex("""
+    mybuffer = """
     0F000044EEEE0040
     F71F4362455BC55BA89A8FAF018288EC
 00B32717482E7624B257D9797C8FF602 7996D84627609FF8625637DFAEF4A648 C4A3517CA65E5BA3794DC5997839EF1A 
-    """)
+    """
 
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
@@ -291,20 +289,20 @@ async def test_certificate_verify():
 
 async def test_finished():
     reader = HandshakeParser()
-    verify_data = bytes.fromhex("""
+    verify_data = """
     2A75BE8DB1281820C3E91C3ACFB356E5
     38BDC640DA0A81635986F3D28C391521
-    """)
+    """
 
-    mybuffer = bytes.fromhex("""
+    mybuffer = """
         14000020
         2A75BE8DB1281820C3E91C3ACFB356E5
         38BDC640DA0A81635986F3D28C391521 
-        """)
+        """
 
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
-    expected = Variant(0x14, {"verify_data": verify_data})
+    expected = Variant(0x14, {"verify_data": bytearray.fromhex(verify_data)})
     compare_result(res, expected)
 
 
@@ -327,14 +325,13 @@ async def test_finished():
 async def test_handshake():
     reader = HandshakeParser()
 
-    mybuffer = bytearray.fromhex(
-        """
+    mybuffer = """
         01 00 00 40 03 03 93 3E A2 1E C3 80 2A 56 15 50
         EC 78 D6 ED 51 AC 24 39 D7 E7 49 C3 1B C3 A3 45
         61 65 88 96 84 CA 00 00 04 FF 88 FF 89 01 00 00
         13 00 0D 00 06 00 04 EE EE EF EF FF 01 00 01 00
         00 17 00 00
-        """)
+        """
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
     print(res)
@@ -357,21 +354,20 @@ async def test_handshake():
 
     print(res.update_size())
 
-    buf = new_io_bytes_from_string()
+    buf = io.BytesIO()
 
     print(res.write(buf))
     buf.seek(0)
     print(binascii.hexlify(buf.read(res.get_full_size())))
-    print(binascii.hexlify(mybuffer))
+    print(binascii.hexlify(bytearray.fromhex(mybuffer)))
 
-    mybuffer = bytearray.fromhex(
-        """
+    mybuffer = """
         02 00 00 41 03 03 93 3E A2 1E 49 C3 1B C3 A3 45
         61 65 88 96 84 CA A5 57 6C E7 92 4A 24 F5 81 13
         80 8D BD 9E F8 56 10 C3 80 2A 56 15 50 EC 78 D6
         ED 51 AC 24 39 D7 E7 FF 88 00 00 09 FF 01 00 01
         00 00 17 00 00
-        """)
+        """
 
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
@@ -390,105 +386,103 @@ async def test_handshake():
 
     print(res.update_size())
 
-    buf = new_io_bytes_from_string()
+    buf = io.BytesIO()
 
     print(res.write(buf))
     buf.seek(0)
 
     print(binascii.hexlify(buf.read(res.get_full_size())))
-    print(binascii.hexlify(mybuffer))
+    print(binascii.hexlify(bytearray.fromhex(mybuffer)))
 
-    mybuffer = bytearray.fromhex(
-        """ 
+    mybuffer = """ 
         0B 00 02 BC 00 02 B9 00 02 B6 30 82 02 B2 30 82
-    
+
         02 61 A0 03 02 01 02 02 0A 28 A2 90 E3 00 00 00
-    
+
         D8 D1 42 30 08 06 06 2A 85 03 02 02 03 30 3A 31
-    
+
         12 30 10 06 0A 09 92 26 89 93 F2 2C 64 01 19 16
-    
+
         02 72 75 31 12 30 10 06 0A 09 92 26 89 93 F2 2C
-    
+
         64 01 19 16 02 63 70 31 10 30 0E 06 03 55 04 03
-    
+
         13 07 74 65 73 74 2D 63 61 30 1E 17 0D 31 37 31
-    
+
         30 32 34 30 32 35 30 35 36 5A 17 0D 32 37 31 30
-    
+
         32 34 30 39 33 30 35 36 5A 30 21 31 1F 30 1D 06
-    
+
         03 55 04 03 13 16 53 65 72 76 65 72 54 4C 53 31
-    
+
         32 54 65 73 74 53 61 6D 70 6C 65 73 30 68 30 21
-    
+
         06 08 2A 85 03 07 01 01 01 01 30 15 06 09 2A 85
-    
+
         03 07 01 02 01 01 01 06 08 2A 85 03 07 01 01 02
-    
+
         02 03 43 00 04 40 FD 13 E3 20 DC 43 F4 71 23 60
-    
+
         E1 1F 8A 50 E0 94 07 47 45 72 12 E9 56 6E 02 CB
-    
+
         4C 60 E3 D6 3E C0 EC 25 10 9A E3 99 C7 69 49 6D
-    
+
         A4 89 29 85 1A 8D 9C 47 C8 FA 0A 8E E7 20 B7 DB
-    
+
         A2 91 94 57 4D 99 A3 82 01 59 30 82 01 55 30 13
-    
+
         06 03 55 1D 25 04 0C 30 0A 06 08 2B 06 01 05 05
-    
+
         07 03 01 30 0E 06 03 55 1D 0F 01 01 FF 04 04 03
-    
+
         02 04 F0 30 1D 06 03 55 1D 0E 04 16 04 14 B0 90
-    
+
         04 86 FC 71 C5 91 5A CA 9B 6B 36 1C 18 A8 37 14
-    
+
         35 1B 30 1F 06 03 55 1D 23 04 18 30 16 80 14 9E
-    
+
         03 F0 B8 9C FC 60 DC 8A 18 1E E8 00 DF A8 5B 32
-    
+
         CD 73 76 30 3F 06 03 55 1D 1F 04 38 30 36 30 34
-    
+
         A0 32 A0 30 86 2E 68 74 74 70 3A 2F 2F 76 6D 2D
-    
+
         74 65 73 74 2D 63 61 2E 63 70 2E 72 75 2F 43 65
-    
+
         72 74 45 6E 72 6F 6C 6C 2F 74 65 73 74 2D 63 61
-    
+
         2E 63 72 6C 30 81 AC 06 08 2B 06 01 05 05 07 01
-    
+
         01 04 81 9F 30 81 9C 30 4B 06 08 2B 06 01 05 05
-    
+
         07 30 02 86 3F 68 74 74 70 3A 2F 2F 76 6D 2D 74
-    
+
         65 73 74 2D 63 61 2E 63 70 2E 72 75 2F 43 65 72
-    
+
         74 45 6E 72 6F 6C 6C 2F 76 6D 2D 74 65 73 74 2D
-    
+
         63 61 2E 63 70 2E 72 75 5F 74 65 73 74 2D 63 61
-    
+
         2E 63 72 74 30 4D 06 08 2B 06 01 05 05 07 30 02
-    
+
         86 41 66 69 6C 65 3A 2F 2F 5C 5C 76 6D 2D 74 65
-    
+
         73 74 2D 63 61 2E 63 70 2E 72 75 5C 43 65 72 74
-    
+
         45 6E 72 6F 6C 6C 5C 76 6D 2D 74 65 73 74 2D 63
-    
+
         61 2E 63 70 2E 72 75 5F 74 65 73 74 2D 63 61 2E
-    
+
         63 72 74 30 08 06 06 2A 85 03 02 02 03 03 41 00
-    
+
         93 30 50 11 50 42 80 3B 6F DD 1D 99 6A 75 0A C8
-    
+
         DA 2C 3E F2 28 47 5E D3 FB C7 9A 3E A1 C7 D5 80
-    
+
         AE 08 D0 81 F3 14 B4 88 09 BD 2C D4 B5 8F A8 4C
-    
+
         B2 B6 66 11 FD 6C 0A 84 BE 59 25 3D 18 87 CC 02
         """
-    )
 
     mybufferstream = new_io_bytes_from_string(mybuffer)
     res = await reader(mybufferstream)
@@ -497,11 +491,11 @@ async def test_handshake():
 
     print(res)
 
-    buf = new_io_bytes_from_string()
+    buf = io.BytesIO()
 
     print(res.write(buf))
     buf.seek(0)
-    assert buf.read(res.get_full_size()) == mybuffer
+    assert buf.read(res.get_full_size()) == bytearray.fromhex(mybuffer)
 
 
 if __name__ == "__main__":
