7c37f124e0b7476ce355c7e606233039ef95a7c7
Sergey Grigoryants <isergey.am@gmail.com>
2023-05-15 18:28:19 +0300

    Record updated

--- record/record_protocol.py
+++ record/record_protocol.py
@@ -20,12 +20,8 @@ def exception_guard(message_on_exit: str = 'exit'):
         return True  # indicates that exception was gracefully handled
 
 
-global mac_data
-
-
-class Record:
+class RecordBase:
     def __init__(self):
-        self.type = 0
         self.version = 0x0303
         self.seqnum = 0
 
@@ -33,40 +29,72 @@ class Record:
         self.Kmac = None
         self.Kenc = None
         self.IV = None
-        self.mac_tls_tree = None
-        self.enc_tls_tree = None
         self.mac_kaznechik_on_tree = None
         self.enc_kaznechik_on_tree = None
-        # по идее ключи и IV должны откуда то приходить
+
+    def mac_data(self, tp, length, fragment):
+        data = (bytearray(self.seqnum.to_bytes(8, 'big')) +
+                bytearray(tp.to_bytes(1, 'big')) +
+                bytearray(self.version.to_bytes(2, 'big')) +
+                bytearray(length.to_bytes(2, 'big')) +
+                fragment
+                )
+        return data
 
     def set_keys(self, Kmac, Kenc, IV):
         self.Kmac = Kmac
         self.Kenc = Kenc
         self.IV = IV
-        self.mac_tls_tree = newTLSTreeKuznechik(Kmac)
-        self.enc_tls_tree = newTLSTreeKuznechik(Kenc)
         self.mac_kaznechik_on_tree = KuznechikOnTree(Kmac)
         self.enc_kaznechik_on_tree = KuznechikOnTree(Kenc)
         self.cipher = True
         self.seqnum = 0
 
-    def mac_data(self, length, fragment):
-        MACData = (bytearray(self.seqnum.to_bytes(8, 'big')) +
-                   bytearray(self.type.to_bytes(1, 'big')) +
-                   bytearray(self.version.to_bytes(2, 'big')) +
-                   bytearray(length.to_bytes(2, 'big')) +
-                   fragment
-                   )
-        return MACData
 
-    def write_records(self, type: int, message: bytes, writer: asyncio.StreamWriter):
-        self.type = type
+class RecordReaderSimple(RecordBase):
+    def __init__(self):
+        super().__init__()
+
+    async def read_record(self, reader: asyncio.StreamReader) -> (int, bytes):
+        try:
+            record_type = int.from_bytes(await reader.readexactly(1), 'big')
+            # assert type == 0x23
+            version = int.from_bytes(await reader.readexactly(2), 'big')
+            assert version == 0x0303
+            length = int.from_bytes(await reader.readexactly(2), 'big')
+            fragment = await reader.readexactly(length)
+            if not self.cipher:
+                self.seqnum += 1
+                return record_type, fragment
+            else:
+                omac = OMAC(self.mac_kaznechik_on_tree(self.seqnum), 128)
+                IV = bytearray(((int.from_bytes(self.IV, 'big') + self.seqnum) % pow(2, 64)).to_bytes(8, 'big'))
+                RecEnc = CtrAcpkm(self.enc_kaznechik_on_tree(self.seqnum), 256, 128).decode(IV, fragment)
+                fragment = RecEnc[:-16]
+                length = len(fragment)
+                mac = RecEnc[-16:]
+                MACData = self.mac_data(record_type, length, fragment)
+                in_mac = omac.mac(MACData)
+                assert in_mac == mac
+                self.seqnum += 1
+                return record_type, fragment
+
+        except:
+            # print_exc()
+            return 0, bytes()
+
+
+class RecordWriterSimple(RecordBase):
+    def __init__(self):
+        super().__init__()
+
+    def write_records(self, record_type: int, message: bytes, writer: asyncio.StreamWriter):
         n = ceil(len(message) / 2 ** 14)
         if not self.cipher:
             for i in range(n):
                 fragment = message[i * 2 ** 14: (i + 1) * 2 ** 14]
                 length = len(fragment)
-                writer.write(bytearray(self.type.to_bytes(1, 'big')) +
+                writer.write(bytearray(record_type.to_bytes(1, 'big')) +
                              bytearray(self.version.to_bytes(2, 'big')) +
                              bytearray(length.to_bytes(2, 'big')) +
                              fragment)
@@ -75,9 +103,7 @@ class Record:
             for i in range(n):
                 fragment = message[i * 2 ** 14: (i + 1) * 2 ** 14]
                 length = len(fragment)
-                global mac_data
-                MACData = self.mac_data(length, fragment)
-                mac_data = MACData
+                MACData = self.mac_data(record_type, length, fragment)
 
                 omac = OMAC(self.mac_kaznechik_on_tree(self.seqnum), 128)
                 RecMac = omac.mac(MACData)
@@ -87,128 +113,51 @@ class Record:
                 RecEnc = CtrAcpkm(self.enc_kaznechik_on_tree(self.seqnum), 256, 128).encode(IV, EncData)
                 length = len(RecEnc)
 
-                writer.write(bytearray(self.type.to_bytes(1, 'big')) +
+                writer.write(bytearray(record_type.to_bytes(1, 'big')) +
                              bytearray(self.version.to_bytes(2, 'big')) +
                              bytearray(length.to_bytes(2, 'big')) +
                              RecEnc
                              )
                 self.seqnum += 1
 
-    async def read_record(self, reader: asyncio.StreamReader) -> (int, bytes):
-        try:
-            type = int.from_bytes(await reader.readexactly(1), 'big')
-            self.type = type
-            # assert type == 0x23
-            version = int.from_bytes(await reader.readexactly(2), 'big')
-            assert version == 0x0303
-            length = int.from_bytes(await reader.readexactly(2), 'big')
-            fragment = await reader.readexactly(length)
-            if not self.cipher:
-                self.seqnum += 1
-                return type, fragment
-            else:
-                omac = OMAC(self.mac_kaznechik_on_tree(self.seqnum), 128)
-                IV = bytearray(((int.from_bytes(self.IV, 'big') + self.seqnum) % pow(2, 64)).to_bytes(8, 'big'))
-                RecEnc = CtrAcpkm(self.enc_kaznechik_on_tree(self.seqnum), 256, 128).decode(IV, fragment)
-                fragment = RecEnc[:-16]
-                length = len(fragment)
-                mac = RecEnc[-16:]
-                MACData = self.mac_data(length, fragment)
-                in_mac = omac.mac(MACData)
-                assert in_mac == mac
-                self.seqnum += 1
-                return type, fragment
 
-        except:
-            # print_exc()
-            return 0, bytes()
-
-
-class RecordReaderWrapper:
-    def __init__(self, read_buffer, record_type):
-        self.read_buffer = read_buffer
-        self.record_type = record_type
+class RecordReader:
+    def __init__(self, io_reader: asyncio.StreamReader):
+        self.record_reader = RecordReaderSimple()
         self.pos = 0
+        self.type = None
+        self.cur_buffer = bytearray()
+        self.io_reader = io_reader
 
     async def read(self, size):
         self.pos += size
-        return await self.read_buffer(self.record_type, size)
+        buf = bytearray()
+        while size != 0:
+            if size > len(self.cur_buffer):
+                size -= len(self.cur_buffer)
+                buf += self.cur_buffer
+                self.type, self.cur_buffer = await self.record_reader.read_record(self.io_reader)
+            else:
+                buf += self.cur_buffer[:size]
+                self.cur_buffer = self.cur_buffer[size:]
+                size = 0
+        return buf
 
     def tell(self):
         return self.pos
 
+    def get_type(self):
+        return self.type
 
-class RecordWriterWrapper:
-    def __init__(self, record_writer, record_type):
-        self.record_writer = record_writer
+
+class RecordWriter:
+    def __init__(self, record_type: int, io_writer: asyncio.StreamWriter):
         self.record_type = record_type
+        self.io_writer = io_writer
+        self.record_writer = RecordWriterSimple()
 
     def write(self, message):
-        self.record_writer(self.record_type, message)
-
-
-class RecordAlternative:
-
-    def __init__(self, reader, writer):
-        self.reader = reader
-        self.writer = writer
-        self.handshake_buffer = io.BytesIO()
-        self.appdata_buffer = io.BytesIO()
-        self.version = 0x0303
-        self.cipher = False
-        self.input = []
-        self.output = []
-
-    async def read_buffer(self, record_type, size):
-        result = bytearray()
-        current = 0
-        if record_type == 22:
-            while current != size:
-                result.extend(self.handshake_buffer.read(size))
-                if len(result) == current:
-                    await self.read_record()
-                current = len(result)
-        if record_type == 23:
-            while current != size:
-                result.extend(self.appdata_buffer.read(size))
-                if len(result) == current:
-                    await self.read_record()
-                current = len(result)
-        return result
-
-    async def read_record(self):
-        record_type = int.from_bytes(await self.reader.readexactly(1), 'big')
-        version = int.from_bytes(await self.reader.readexactly(2), 'big')
-        assert version == 0x0303
-        length = int.from_bytes(await self.reader.readexactly(2), 'big')
-        fragment = await self.reader.readexactly(length)
-        if record_type == 22:
-            append_buffer(self.handshake_buffer, fragment)
-
-        if record_type == 23:
-            append_buffer(self.appdata_buffer, fragment)
-
-    def record_writer(self, record_type, message):
-        n = floor(len(message) / 2 ** 14)
-        for i in range(n):
-            self.send_fragment(record_type, message[i * 2 ** 14: (i + 1) * 2 ** 14])
-        if len(message) % 2 ** 14:
-            self.send_fragment(record_type, message[n * 2 ** 14:])
-
-    def send_fragment(self, record_type, fragment):
-        if not self.cipher:
-            plain = bytearray(record_type.to_bytes(1, 'big')) + \
-                    bytearray(self.version.to_bytes(2, 'big')) + \
-                    bytearray(len(fragment).to_bytes(2, 'big')) + \
-                    fragment
-            self.output.append(plain)
-            self.writer.write(plain)
-
-    def handshaker(self):
-        return RecordReaderWrapper(self.read_buffer, 22), RecordWriterWrapper(self.record_writer, 22)
-
-    def appdata_rw(self):
-        return RecordReaderWrapper(self.read_buffer, 23), RecordWriterWrapper(self.record_writer, 23)
+        self.record_writer.write_records(self.record_type, message, self.io_writer)
 
 
 num_messages = 3
@@ -223,21 +172,13 @@ IV = bytearray.fromhex('12 34 56 78 90 AB CE F0')
 
 async def handle_read_records(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     global server
-    idx = 0
-    rec = Record()
-    rec.set_keys(K, K, IV)
-    message = bytearray()
-    while True:
-        type, fragment = await rec.read_record(reader)
-        message += fragment
-        if len(message) == message_len:
-            print(f"Received {len(message)} fragment")
-            assert message == my_messages[idx]
-            message = bytearray()
-            idx += 1
-            if idx == num_messages:
-                server.close()
-                return
+    rec = RecordReader(reader)
+    rec.record_reader.set_keys(K, K, IV)
+    for expected_message in my_messages:
+        message = await rec.read(message_len)
+        assert message == expected_message
+        print("Received message")
+    server.close()
 
 
 async def start_server():
@@ -254,11 +195,11 @@ async def start_server():
 
 
 async def client():
-    rec = Record()
-    rec.set_keys(K, K, IV)
     reader, writer = await asyncio.open_connection('localhost', 8888)
-    for i in range(num_messages):
-        rec.write_records(0x23, my_messages[i], writer)
+    rec = RecordWriter(0x23, writer)
+    rec.record_writer.set_keys(K, K, IV)
+    for message in my_messages:
+        rec.write(message)
     writer.close()
 
 
